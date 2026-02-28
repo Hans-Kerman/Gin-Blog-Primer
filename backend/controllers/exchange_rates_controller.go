69b295bf0f7f5d8bf7aa82e9680c1846b8ce846b
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-03-01 00:37:28 +0800

    feat:完成汇率数据的存储和读取逻辑

---
+++ backend/controllers/exchange_rates_controller.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Hans-Kerman/GinBlogPrimer/backend/global"
+	"github.com/Hans-Kerman/GinBlogPrimer/backend/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateExchangeRate(ctx *gin.Context) {
+	var exchangeRate models.ExchangeRate
+
+	if err := ctx.ShouldBindJSON(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	exchangeRate.Date = time.Now()
+
+	if err := global.Db.AutoMigrate(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := global.Db.Create(&exchangeRate).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, exchangeRate)
+}
+
+func GetExchangeRates(ctx *gin.Context) {
+	var exchangeRates []models.ExchangeRate
+
+	if err := global.Db.Find(&exchangeRates).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, exchangeRates)
+}
