f4e232686712ab875acc6cd24f8d46cae674a178
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-03-03 00:53:04 +0800

    feat:完成Redis旁路缓存模式的文章存取

--- backend/controllers/article_controller.go
+++ backend/controllers/article_controller.go
@@ -1,14 +1,19 @@
 package controllers
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/Hans-Kerman/GinBlogPrimer/backend/global"
 	"github.com/Hans-Kerman/GinBlogPrimer/backend/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"gorm.io/gorm"
 )
+
+var cacheKey = "articles"
 
 func CreateArticle(ctx *gin.Context) {
 	var article models.Article
@@ -38,22 +43,56 @@ func CreateArticle(ctx *gin.Context) {
 }
 
 func GetArticles(ctx *gin.Context) {
-	var articles []models.Article
 
-	if err := global.Db.Find(&articles).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			ctx.JSON(http.StatusNotFound, gin.H{
-				"error": err.Error(),
-			})
-		} else {
+	cachedData, err := global.RedisDB.Get(cacheKey).Result()
+
+	if errors.Is(err, redis.Nil) { //缓存未命中
+		var articles []models.Article
+
+		if err := global.Db.Find(&articles).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": err.Error(),
+				})
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": err.Error(),
+				})
+			}
+			return
+		}
+
+		articleJSON, err := json.Marshal(articles)
+		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
 			})
+			return
 		}
-		return
-	}
 
-	ctx.JSON(http.StatusOK, articles)
+		if err := global.RedisDB.Set(cacheKey, articleJSON, time.Minute*10).Err(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	} else if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	} else {
+		var articles []models.Article
+
+		err := json.Unmarshal([]byte(cachedData), &articles)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, articles)
+	}
 }
 
 func GetArticlesByID(ctx *gin.Context) {
