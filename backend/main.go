6efbe84525392a6e0bf42110babbe1e6b97c636c
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-03-05 19:45:35 +0800

    feat:加入优雅退出功能

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/Hans-Kerman/GinBlogPrimer/backend/config"
 	"github.com/Hans-Kerman/GinBlogPrimer/backend/router"
 )
@@ -15,5 +22,26 @@ func main() {
 		port = ":8080"
 	}
 
-	r.Run(port)
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit //在这里阻塞住主goroutine不退出
+	log.Println("Shutdown Server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
